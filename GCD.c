5b460f86a266367fac8d16c474da463bd214d3ea
Chaithu004 <133729885+Chaithu004@users.noreply.github.com>
2024-06-11 10:58:36 +0530

    Add files via upload

---
+++ GCD.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	int i,n,m,GCD;
+	printf("B.chaithanya, Reg.No - 192210331");
+	printf("\nEnter the Number:");
+	scanf("%d %d",&m,&n);
+	for(i=1;i<=n && i<=m; i++){
+		if(n%i==0 && m%i==0){
+			GCD = i;
+		}
+	}
+	printf("GCD of the numbers %d and %d is %d",n,m,GCD);
+	return 0;
+}
